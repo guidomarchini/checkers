2c4dbf7023f7683ab4f21ed6b5be18f28304d86a
= <=>
2020-05-18 00:47:10 -0300

    fixed bug with matches id

--- src/main/persistence/repository/matchRepository.js
+++ src/main/persistence/repository/matchRepository.js
@@ -8,9 +8,15 @@ const path = require('path');
 const MATCH = 'match';
 const dirpath = path.join(`${__dirname}/out/${MATCH}`);
 
+// a match creates 3 links: spectator, white checkers player, black checkers player.
+// spectator link matches its match id, and the other 2 are the ones that follows.
+// the match has to delegate its id creation to the links.
+// I know, it's ugly. I know I can do this better
+const idEntity = 'link';
+
 module.exports = {
     createMatch : function(firstTurn) {
-        const newMatchId = idGenerator.generateId(MATCH);
+        const newMatchId = idGenerator.generateId(idEntity);
         const newMatch = new MatchEntity(newMatchId, firstTurn, new Date());
         fs.writeFileSync(`${dirpath}/${newMatchId}`, JSON.stringify(newMatch));
         return newMatch;
